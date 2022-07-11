28c5fde3351c69aaca4dd181d6a0e48634456b87
Chris Maze <boomaze@gmail.com>
2022-07-11 15:22:15 -0400

    init

---
+++ FrieghtConfig_548/New sprocs/fnGetPO_NOTFULLYBILLED.sql
@@ -0,0 +1,74 @@
+USE [TMS]
+GO
+
+/****** Object:  UserDefinedFunction [dbo].[fnGetPO_NOTFULLYBILLED]    Script Date: 7/11/2022 12:50:24 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+/* =================================================================
+ Author:	  Johana Aleman
+ Create date: 03/07/2016
+ Description: Returns a dataset with the PO_NUMBER, LO_PURCHASE_ORDER_ID  
+			  AND LO_LOAD_NUMBER that has not been Fully Billed
+ Modification Log:
+  10/26/2018  JAleman  Change the logic to make the function dynamic
+					   if the Load Carrier <> Bill POs Carrier then
+					   the Load/PO is not fully Billed.
+
+  10/4/2019	  		   Modified Where clause to filter out shuttle
+                       carries by CARRIER_ID.
+
+  12/11/2019		   Modified Where clause to filter out any PO 
+					   that is on a load where the Carrier has 
+					   CR_CARRIERS.SHUTTLE_CARRIER = 1. 
+					   
+  12/30/2019		   Modified Where clause to filter out the carriers
+					   who are shuttle carriers 
+					   CR_CARRIERS.SHUTTLE_CARRIER <> 1. 
+					   CR_CARRIERS.SHUTTLE_CARRIER = 1 was limiting the
+					   recordset to only shuttle carriers not filtering
+					   them out.					   	
+ =================================================================*/
+ALTER FUNCTION [dbo].[fnGetPO_NOTFULLYBILLED]
+(
+	@StartDate DATE
+,	@EndDate DATE
+) RETURNS TABLE
+AS
+RETURN
+(
+	SELECT POS.PO_NUMBER,
+		POS.LO_PURCHASE_ORDER_ID,
+		LPOS.LO_LOAD_NUMBER
+	FROM LO_PURCHASE_ORDERS POS 
+		INNER JOIN LO_PO_LOADS LPOS 
+			ON POS.LO_PURCHASE_ORDER_ID = LPOS.LO_PURCHASE_ORDER_ID
+				AND (LPOS.IS_DELETED IS NULL OR LPOS.IS_DELETED = 0)
+		INNER JOIN LO_LOADS LDS 
+			ON LPOS.LO_LOAD_NUMBER = LDS.LO_LOAD_NUMBER
+				AND LDS.TMS_STATUS_ID NOT IN (32)
+		LEFT JOIN (SELECT BPOS.LO_PURCHASE_ORDER_ID, bb.BL_BILLING_ID
+						, BPOS.LO_LOAD_NUMBER, bb.CR_CARRIER_ID
+					FROM BL_BILL_POS BPOS 
+						INNER JOIN BL_BILLING bb 
+							ON BPOS.BL_BILLING_ID = bb.BL_BILLING_ID
+								AND (BPOS.IS_DELETED IS NULL OR BPOS.IS_DELETED = 0)
+					WHERE bb.INVOICE_AMOUNT > 0) VALIDBILLS
+			ON POS.LO_PURCHASE_ORDER_ID = VALIDBILLS.LO_PURCHASE_ORDER_ID
+				AND LPOS.LO_LOAD_NUMBER = VALIDBILLS.LO_LOAD_NUMBER
+				AND LDS.CR_CARRIER_ID = VALIDBILLS.CR_CARRIER_ID
+		INNER JOIN dbo.CR_CARRIERS AS CR
+		    ON LDS.CR_CARRIER_ID = CR.CR_CARRIER_ID
+	WHERE POS.TMS_STATUS_ID = 9
+		AND VALIDBILLS.BL_BILLING_ID IS NULL
+		AND LDS.CR_CARRIER_ID NOT IN (78, 1680, 2496, 1704)
+		AND (CAST(POS.REC_DATE AS DATE) BETWEEN @StartDate AND @EndDate)		
+		--AND LDS.CR_CARRIER_ID NOT IN (2116,4103,4569,4601,4917,5140,5144,5307,7135,7449,7518)-- Separating bc this will be replace during LCS project with CR_CARRIERS.SHUTTLE_CARRIER logic
+		AND CR.SHUTTLE_CARRIER <> 1
+)
+GO
+
+
---
+++ FrieghtConfig_548/New sprocs/fnGetSuppliersPOExpenses.sql
@@ -0,0 +1,56 @@
+USE [TMS]
+GO
+
+/****** Object:  UserDefinedFunction [dbo].[fnGetSuppliersPOExpenses]    Script Date: 7/11/2022 12:55:37 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		<Johana Aleman>
+-- Create date: <04/03/2016>
+-- Description:	<Returns a table with all the PO calculation expenses between the start and end date (LO_PURCHASE_ORDERS.REC_DATE) >
+-- =============================================
+ALTER FUNCTION [dbo].[fnGetSuppliersPOExpenses] 
+(	
+	-- Add the parameters for the function here
+	@StarDate DATE,
+	@EndDate DATE
+)
+RETURNS TABLE 
+AS
+RETURN 
+(
+		SELECT DISTINCT
+		CASE
+			WHEN SUM(lpo.TOTAL_WEIGHT) OVER (PARTITION BY bb.BL_BILLING_ID) > 0
+			THEN (lpo.TOTAL_WEIGHT / SUM(lpo.TOTAL_WEIGHT) OVER (PARTITION BY bb.BL_BILLING_ID)) * bb.INVOICE_AMOUNT
+			ELSE NULL
+		END AS FREIGHT_PAID_PO
+	,	lpo.LO_PURCHASE_ORDER_ID
+	,	bbp.LO_LOAD_NUMBER
+	,	bb.BL_BILLING_ID
+	,	lpo.PO_NUMBER
+	FROM LO_PURCHASE_ORDERS lpo WITH(NOLOCK)
+		
+		INNER JOIN dbo.BL_BILL_POS bbp WITH(NOLOCK)
+				ON bbp.LO_PURCHASE_ORDER_ID = lpo.LO_PURCHASE_ORDER_ID
+					AND ISNULL(bbp.IS_DELETED, 0) = 0
+		INNER JOIN dbo.BL_BILLING bb WITH(NOLOCK)
+				ON bb.BL_BILLING_ID = bbp.BL_BILLING_ID
+				AND bb.CR_CARRIER_ID NOT IN (78, 1680, 2629, 69, 2616, 3122)
+	WHERE
+		bb.BL_BILLING_ID IN (SELECT bbp.BL_BILLING_ID
+			FROM LO_PURCHASE_ORDERS lpo WITH(NOLOCK)
+				INNER JOIN BL_BILL_POS bbp WITH(NOLOCK)
+						ON lpo.LO_PURCHASE_ORDER_ID = bbp.LO_PURCHASE_ORDER_ID
+							AND ISNULL(bbp.IS_DELETED, 0) = 0
+			WHERE CAST(lpo.REC_DATE AS DATE) BETWEEN @StarDate AND @EndDate)
+		AND lpo.TMS_STATUS_ID != 8
+
+)
+GO
+
+
